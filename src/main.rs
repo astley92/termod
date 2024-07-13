8b41fd70644aabb64b89296967008412fc789e9e
Blake Astley <astley92@hotmail.com>
2024-07-13 15:14:31 +1000

    Show frame_count

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,18 @@ use crossterm::event;
 use crossterm::{QueueableCommand, ExecutableCommand};
 use std::io::{stdout, Write};
 
+
 struct AppState {
     pub running: bool,
-    pub frame_count: u32,
+    pub frame_count: u16,
 }
 
 struct Buffer {
     pub size: u16,
     pub chars: Vec<Character>,
     pub curs_pos: u16,
-    pub width: u16,
-    pub height: u16,
+    width: u16,
+    height: u16,
 }
 
 impl Buffer {
@@ -42,9 +43,37 @@ impl Buffer {
 
     pub fn insert_ch(&mut self, ch: Character) {
         self.chars[self.curs_pos as usize] = ch;
+        self.increment_cursor();
     }
 
-    pub fn increment_cursor(&mut self) {
+    pub fn insert_chars(&mut self, chars: Vec<Character>) {
+        for ch in chars {
+            self.chars[self.curs_pos as usize] = ch;
+            self.increment_cursor();
+        };
+    }
+
+    pub fn move_cursor_to_x_y(&mut self, x: u16, y: u16) -> u16 {
+        if x >= self.width { 
+            panic!("Cursor moved outside of buffer width given x {} buffer width {}", x, self.width);
+        } else if y >= self.height {
+            panic!("Cursor moved outside of buffer height");
+        };
+        
+        let result = utils::linear_pos_from_x_y(x, y, self.width);
+        self.curs_pos = result;
+        return result;
+    }
+
+    pub fn move_cursor_to(&mut self, pos: u16) {
+        if pos >= self.size {
+            panic!("Cursor moved outside buffer range");
+        };
+
+        self.curs_pos = pos;
+    }
+
+    fn increment_cursor(&mut self) {
         if self.curs_pos >= self.size - 1 {
             self.curs_pos -= 1;
         }
@@ -53,7 +82,7 @@ impl Buffer {
 }
 
 #[derive(Clone, Debug)]
-struct Character {
+pub struct Character {
     character: u8,
     attributes: Vec<style::Attribute>,
 }
@@ -69,6 +98,23 @@ impl Character {
     }
 }
 
+
+mod utils {
+    pub fn linear_pos_from_x_y(x: u16, y: u16, width: u16) -> u16 {
+        let adjusted_y = y * width;
+        return x + adjusted_y;
+    }
+
+    pub fn char_vec_from_u16(num: u16) -> Vec<crate::Character> {
+        let num_str = num.to_string();
+        let mut result = vec![];
+        for ch in num_str.chars() {
+            result.push(crate::Character::from_char(ch));
+        };
+        return result;
+    }
+}
+
 const FPS: u16 = 30;
 
 fn main() {
@@ -80,7 +126,7 @@ fn main() {
     let mut stdout = stdout();
     stdout.execute(terminal::EnterAlternateScreen).unwrap();
     terminal::enable_raw_mode().unwrap();
-    stdout.execute(cursor::Hide);
+    stdout.execute(cursor::Hide).unwrap();
 
     let (w_width, w_height) = terminal::size().unwrap();
     let mut buffer = Buffer::empty(w_width, w_height);
@@ -100,15 +146,19 @@ fn main() {
                 }
                 let character = Character::from_char(ch);
                 buffer.insert_ch(character.clone());
-                buffer.increment_cursor();
             };
         };
         
         // Update
         state.frame_count += 1;
+        let last_pos = buffer.curs_pos;
+        buffer.move_cursor_to_x_y(0, buffer.height - 1);
+        buffer.insert_chars(utils::char_vec_from_u16(state.frame_count));
+        buffer.move_cursor_to(last_pos);
 
         // Draw
         stdout.queue(terminal::Clear(terminal::ClearType::Purge)).unwrap();
+        
 
         for i in 0..buffer.chars.len() {
             let character = &buffer.chars[i];
@@ -124,7 +174,7 @@ fn main() {
         stdout.flush().unwrap();
     }
     
-    stdout.execute(cursor::Show);
+    stdout.execute(cursor::Show).unwrap();
     terminal::disable_raw_mode().unwrap();
     stdout.execute(terminal::LeaveAlternateScreen).unwrap(); 
 } 
