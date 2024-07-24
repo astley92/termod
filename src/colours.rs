5deb7a44b51e78295668eee791f1f7432f7b8489
Blake Astley <astley92@hotmail.com>
2024-07-24 23:14:42 +1000

    More todo functionality

--- src/colours.rs
+++ src/colours.rs
@@ -3,3 +3,4 @@ use crossterm::style;
 pub const GREY: style::Color = style::Color::Rgb { r: 100, g: 100, b: 100 };
 pub const LIGHT_GREY: style::Color = style::Color::Rgb { r: 200, g: 200, b: 200 };
 pub const ORANGE: style::Color = style::Color::Rgb { r: 245, g: 158, b: 66 };
+pub const LIGHT_BLUE: style::Color = style::Color::Rgb { r: 66, g: 152, b: 245 };
--- src/todo_widget.rs
+++ src/todo_widget.rs
@@ -4,6 +4,7 @@ use crate::widget::{Widget, WidgetTrait};
 use crossterm::{event, style};
 use chrono;
 
+
 #[derive(PartialEq)]
 enum Section {
     TODO, 
@@ -13,13 +14,21 @@ enum Section {
 struct TodoItem {
     title: String,
     completed_at: Option<chrono::DateTime<chrono::Utc>>,
+    added_at: chrono::DateTime<chrono::Utc>,
     working: bool,
+}
+
+impl TodoItem {
+    pub fn calculate_age(&self) -> chrono::TimeDelta {
+        return chrono::Utc::now() - self.added_at;
+    }
 }
 
 struct TodoItems {
     active_section: Section,
     todo_items: Vec<TodoItem>,
     done_items: Vec<TodoItem>,
+    removed_items: Vec<TodoItem>,
     highlighted_todo_item: usize,
     highlighted_done_item: usize,
 }
@@ -40,6 +49,7 @@ impl TodoItems {
             active_section: Section::TODO,
             todo_items,
             done_items,
+            removed_items: vec![],
             highlighted_todo_item: 0,
             highlighted_done_item: 0,
         }
@@ -50,6 +60,7 @@ impl TodoItems {
             active_section: Section::TODO,
             todo_items: vec![],
             done_items: vec![],
+            removed_items: vec![],
             highlighted_todo_item: 0,
             highlighted_done_item: 0,
         }
@@ -88,15 +99,35 @@ impl TodoItems {
         }
     }
 
+    pub fn remove_selected_item(&mut self) {
+        if self.active_section == Section::TODO && self.todo_items.len() > 0 {
+            let mut item = self.todo_items.remove(self.highlighted_todo_item);
+            item.working = false;
+            self.removed_items.push(item);
+            if self.todo_items.len() > 0 && self.highlighted_todo_item > self.todo_items.len()-1 {
+                self.highlighted_todo_item -= 1;
+            }
+        } else if self.active_section == Section::DONE && self.done_items.len() > 0 {
+            let item = self.done_items.remove(self.highlighted_done_item);
+            self.removed_items.push(item);
+            if self.done_items.len() > 0 && self.highlighted_done_item > self.done_items.len()-1 {
+                self.highlighted_done_item -= 1;
+            }
+        }
+    }
+
     pub fn toggle_selected_item_completeness(&mut self) {
-        if self.active_section == Section::TODO {
-            let item = self.todo_items.remove(self.highlighted_todo_item);
+        if self.active_section == Section::TODO && self.todo_items.len() > 0 {
+            let mut item = self.todo_items.remove(self.highlighted_todo_item);
+            item.working = false;
+            item.completed_at = Some(chrono::Utc::now());
             self.done_items.insert(0, item);
             if self.todo_items.len() > 0 && self.highlighted_todo_item > self.todo_items.len()-1 {
                 self.highlighted_todo_item -= 1;
             }
-        } else {
-            let item = self.done_items.remove(self.highlighted_done_item);
+        } else if self.active_section == Section::DONE && self.done_items.len() > 0 {
+            let mut item = self.done_items.remove(self.highlighted_done_item);
+            item.completed_at = None;
             self.todo_items.push(item);
             if self.done_items.len() > 0 && self.highlighted_done_item > self.done_items.len()-1 {
                 self.highlighted_done_item -= 1;
@@ -110,6 +141,19 @@ impl TodoItems {
             item.working = !item.working;
         }
     }
+
+    pub fn undo_last_remove(&mut self) {
+        if self.removed_items.len() < 1 {
+            return;
+        };
+
+        let item = self.removed_items.pop().unwrap();
+        if item.completed_at == None {
+            self.todo_items.push(item);
+        } else {
+            self.done_items.push(item);
+        }
+    }
 }
 
 pub struct TodoState {
@@ -117,6 +161,7 @@ pub struct TodoState {
     update_main_buffer: bool,
     items: TodoItems,
     item_seperator_height: usize,
+    ages_last_calculated_at: std::time::Instant,
 }
 
 impl WidgetTrait for Widget<TodoState> {
@@ -130,10 +175,30 @@ impl WidgetTrait for Widget<TodoState> {
 
 fn init(myself: &mut Widget<TodoState>) {
     myself.state.items = TodoItems::new(vec![
-        TodoItem { title: "Buy groceries".to_string(), completed_at: None ,working: false},
-        TodoItem { title: "Answer emails".to_string(), completed_at: None ,working: false},
-        TodoItem { title: "Write code".to_string(), completed_at: Some(chrono::Utc::now()), working: false },
-        TodoItem { title: "Feed dogs".to_string(), completed_at: Some(chrono::Utc::now()), working: false },
+        TodoItem { 
+            title: "Buy groceries".to_string(),
+            added_at: chrono::Utc::now(),
+            completed_at: None,
+            working: false
+        },
+        TodoItem { 
+            title: "Answer emails".to_string(), 
+            added_at: chrono::DateTime::parse_from_rfc3339("2024-04-01T13:39:57Z").unwrap().to_utc(),
+            completed_at: None,
+            working: false
+        },
+        TodoItem { 
+            title: "Write code".to_string(), 
+            added_at: chrono::DateTime::parse_from_rfc3339("2023-04-01T13:39:57Z").unwrap().to_utc(),
+            completed_at: Some(chrono::Utc::now() - std::time::Duration::from_secs(100)), 
+            working: false 
+        },
+        TodoItem { 
+            title: "Feed dogs".to_string(), 
+            added_at: chrono::DateTime::parse_from_rfc3339("2024-04-01T13:39:57Z").unwrap().to_utc(),
+            completed_at: Some(chrono::Utc::now()), 
+            working: false 
+        },
     ]);
 }
 
@@ -150,6 +215,8 @@ fn event(myself: &mut Widget<TodoState>, event_to_handle: &event::Event) {
                 },
                 event::KeyCode::Enter => { myself.state.items.toggle_selected_item_completeness() },
                 event::KeyCode::Char('w') => { myself.state.items.toggle_selected_item_working() },
+                event::KeyCode::Char('d') => { myself.state.items.remove_selected_item() },
+                event::KeyCode::Char('u') => { myself.state.items.undo_last_remove() },
                 _ => {}
             }
         },
@@ -157,7 +224,11 @@ fn event(myself: &mut Widget<TodoState>, event_to_handle: &event::Event) {
     }
 }
 
-fn update(_myself: &mut Widget<TodoState>) {
+fn update(myself: &mut Widget<TodoState>) {
+    if std::time::Instant::now() - myself.state.ages_last_calculated_at > std::time::Duration::from_secs(1) {
+        myself.state.update_main_buffer = true;
+        myself.state.ages_last_calculated_at = std::time::Instant::now();
+    };
 }
 
 fn draw(myself: &mut Widget<TodoState>) {
@@ -168,18 +239,29 @@ fn draw(myself: &mut Widget<TodoState>) {
     // Draw todo items
     let mut i = 0;
     for item in &all_items.todo_items {
-        let attributes= if all_items.highlighted_todo_item == i { 
+        let attributes= if all_items.active_section == Section::TODO && all_items.highlighted_todo_item == i { 
             Some(style::Attributes::from(style::Attribute::Reverse)) 
         } else { 
             None 
         };
 
-        let title_chars = character::Character::vec_from_string(
+        let age: chrono::TimeDelta = item.calculate_age();
+        let age_days= age.num_days();
+        let age_hours= (age - chrono::TimeDelta::days(age_days)).num_hours();
+        let age_minutes= (age - chrono::TimeDelta::days(age_days) - chrono::TimeDelta::hours(age_hours)).num_minutes();
+        let age_seconds = (age - chrono::TimeDelta::days(age_days) - chrono::TimeDelta::hours(age_hours) - chrono::TimeDelta::minutes(age_minutes)).num_seconds();
+        let mut title_prefix_chars= character::Character::vec_from_string(
+            &format!("{:0>3}:{:0>2}:{:0>2}:{:0>2} - ", age_days, age_hours, age_minutes, age_seconds), 
+            Some(colours::GREY), 
+            None,
+        );
+        let mut title_chars = character::Character::vec_from_string(
             &item.title, 
             if item.working { Some(colours::ORANGE) } else { None }, 
             attributes
         );
-        myself.state.main_buffer.insert_char_slice(i * myself.width as usize, &title_chars);
+        title_prefix_chars.append(&mut title_chars);
+        myself.state.main_buffer.insert_char_slice(i * myself.width as usize, &title_prefix_chars);
         i += 1;
     };
 
@@ -190,20 +272,34 @@ fn draw(myself: &mut Widget<TodoState>) {
     // Draw done items
     let mut i = 0;
     for item in &all_items.done_items {
-        let attributes= if all_items.highlighted_done_item == i { 
+        let attributes= if all_items.active_section == Section::DONE && all_items.highlighted_done_item == i { 
             Some(style::Attributes::from(style::Attribute::Reverse)) 
         } else { 
             None 
         };
 
-        let title_chars = character::Character::vec_from_string(
+        let ttc: chrono::TimeDelta = item.completed_at.unwrap() - item.added_at;
+        let ttc_days= ttc.num_days();
+        let ttc_hours= (ttc- chrono::TimeDelta::days(ttc_days)).num_hours();
+        let ttc_minutes= (ttc- chrono::TimeDelta::days(ttc_days) - chrono::TimeDelta::hours(ttc_hours)).num_minutes();
+        let ttc_seconds = (ttc- chrono::TimeDelta::days(ttc_days) - chrono::TimeDelta::hours(ttc_hours) - chrono::TimeDelta::minutes(ttc_minutes)).num_seconds();
+        
+        let mut title_prefix_chars= character::Character::vec_from_string(
+            &format!("{:0>3}:{:0>2}:{:0>2}:{:0>2} - ", ttc_days, ttc_hours, ttc_minutes, ttc_seconds), 
+            Some(colours::GREY), 
+            None,
+        );
+        let mut title_chars = character::Character::vec_from_string(
             &item.title,
             None, 
             attributes
         );
+
+        title_prefix_chars.append(&mut title_chars);
+
         let line_number = i + myself.state.item_seperator_height + 1;
         if line_number < myself.height as usize {
-            myself.state.main_buffer.insert_char_slice(line_number * myself.width as usize, &title_chars);
+            myself.state.main_buffer.insert_char_slice(line_number * myself.width as usize, &title_prefix_chars);
         }
         i += 1;
     };
@@ -230,6 +326,7 @@ pub fn new(width: u16, height: u16, _x: usize, _y: usize) -> Widget<TodoState> {
             update_main_buffer: true,
             item_seperator_height: height as usize - 10,
             items: TodoItems::empty(),
+            ages_last_calculated_at: std::time::Instant::now() - std::time::Duration::from_secs(10),
         }
     };
 }
